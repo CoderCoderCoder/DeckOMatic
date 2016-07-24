cbaad34184877568778e75490ca615e39a4f9e36
Scott Mikula <mikula@gmail.com>
2016-07-24 09:42:50 -0700

    Add the initial projects

---
+++ DeckOMaticShell/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DeckOMaticShell
+{
+    using DeckOMatic;
+
+    public class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
